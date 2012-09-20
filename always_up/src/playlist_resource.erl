aafeda352dfbf6bdab8877ca39ca08919adc2967
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2012-09-20 10:36:50 +0100

    Streams one song once

--- always_up/src/playlist_resource.erl
+++ always_up/src/playlist_resource.erl
@@ -1,6 +1,6 @@
 -module(playlist_resource).
 
--export([init/1, to_html/2]).
+-export([init/1, content_types_provided/2, to_mp3/2]).
 
 -include_lib("webmachine/include/webmachine.hrl").
 
@@ -8,6 +8,17 @@ init([]) ->
 	{ok, undefined}.
 
 
-to_html(ReqData, State) ->
-	Content = "<head><body><h3>Playlistio</h3></body></head>",
+content_types_provided(ReqData, State) ->
+	{[{"audio/mpeg3", to_mp3}], ReqData, State}.
+
+
+to_mp3(ReqData, State) ->
+	File = "song.mp3",
+	{ok, Content} = file:read_file(File),
 	{Content, ReqData, State}.
+
+	
+
+
+
+	
